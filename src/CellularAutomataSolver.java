c201a93fdaef5cb18099be044db7eb33761bba7e
libcolor <97296222+libcolor@users.noreply.github.com>
2021-02-19 09:32:19 +0000

    Change in readme

--- src/CellularAutomataSolver.java
+++ src/CellularAutomataSolver.java
@@ -3,4 +3,4 @@ import java.awt.*;
 import java.util.Stack;
 
 /**
- * The problem is that the history of the walls is not preserved aka once you start knocking
+ * The problem is that the history of the walls is not preserved aka once you start knocking down wall
