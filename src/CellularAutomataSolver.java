d1aed1802017cd80ceb0128ef5dc79e64b39656b
libcolor <97296222+libcolor@users.noreply.github.com>
2021-06-05 10:11:07 +0000

    [X86] Autogenerate complete checks. NFC

--- src/CellularAutomataSolver.java
+++ src/CellularAutomataSolver.java
@@ -85,4 +85,4 @@ public class CellularAutomataSolver {
         while(flag){
             int x = (int)(Math.random()*mat.length);
             int y = (int)(Math.random()*mat[0].length);
-            if(mat[x][
+            if(mat[x][y].getBackgroun
