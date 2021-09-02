2c4d123aa2b51f455220193e55affa45499417be
libcolor <97296222+libcolor@users.noreply.github.com>
2021-09-02 18:56:47 +0000

    add CHANGES

--- src/CellularAutomataSolver.java
+++ src/CellularAutomataSolver.java
@@ -113,4 +113,4 @@ public class CellularAutomataSolver {
         } catch (Exception e) {
         }
         try {
-            if (mat[p.x][p.y + 1].getBackground() == Color.white||mat[p
+            if (mat[p.x][p.y + 1].getBackground() == Color.white||mat[p.x][p.y+1].get
