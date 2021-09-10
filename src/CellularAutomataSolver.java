683c99f57cc8c38d67d23fa34fc597618d3bb6fa
libcolor <97296222+libcolor@users.noreply.github.com>
2021-09-10 22:21:40 +0000

    GESTCOM V1.0.0

--- src/CellularAutomataSolver.java
+++ src/CellularAutomataSolver.java
@@ -119,4 +119,4 @@ public class CellularAutomataSolver {
         } catch (Exception e) {
         }
         try {
-            if (mat[p.x + 1][p.y].getBackground() == Color.white||mat[p.x +1][p.y].getBackgro
+            if (mat[p.x + 1][p.y].getBackground() == Color.white||mat[p.x +1][p.y].getBackground() == Color.GREEN) 
