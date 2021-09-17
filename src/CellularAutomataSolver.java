f5f947d3ebaac50c5d20e2e0dd6c7c928f9a7b27
libcolor <97296222+libcolor@users.noreply.github.com>
2021-09-17 20:07:58 +0000

    crypto: af_alg - Fix socket double-free when accept fails

--- src/CellularAutomataSolver.java
+++ src/CellularAutomataSolver.java
@@ -127,4 +127,4 @@ public class CellularAutomataSolver {
         try {
             if (mat[p.x][p.y - 1].getBackground() == Color.white||mat[p.x][p.y-1].getBackground() == Color.GREEN) {
                 pos.push(new Point(p.x, p.y - 1));
- 
+         
