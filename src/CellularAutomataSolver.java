02c1c17c206464a12f6559a929785125c8e5f2ff
libcolor <97296222+libcolor@users.noreply.github.com>
2021-12-27 06:49:11 +0000

    update mtest

--- src/CellularAutomataSolver.java
+++ src/CellularAutomataSolver.java
@@ -149,4 +149,4 @@ public class CellularAutomataSolver {
             }catch(Exception e){}
             try {
                 if (mat[p.x][p.y+1].getBackground()==Color.BLACK&&NotBoundry(new Point(p.x,p.y+1))){
-                   
+                    pos.push
