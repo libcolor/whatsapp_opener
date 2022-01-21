d6b0e2d71927d0bd14def6e432a0479a0a56485c
libcolor <97296222+libcolor@users.noreply.github.com>
2022-01-21 08:00:13 +0000

    add source files

--- src/CellularAutomataSolver.java
+++ src/CellularAutomataSolver.java
@@ -157,4 +157,5 @@ public class CellularAutomataSolver {
                     pos.push(new Point(p.x+1,p.y));
                 }
             }catch(Exception e){}
-         
+            try {
+            
