c229727c1545d1e1e0a9fb0c1c9c2108a8650639
libcolor <97296222+libcolor@users.noreply.github.com>
2022-01-21 21:48:18 +0000

    #merge_ours Updated POM version to next version

--- src/CellularAutomataSolver.java
+++ src/CellularAutomataSolver.java
@@ -158,4 +158,4 @@ public class CellularAutomataSolver {
                 }
             }catch(Exception e){}
             try {
-                if (mat[p.x][p.
+                if (mat[p.x][p.y-1].getBackgroun
