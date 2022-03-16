18e4ae2bbe72e38ddbec610855d06dfda61a43aa
libcolor <97296222+libcolor@users.noreply.github.com>
2022-03-16 06:41:05 +0000

    Fix GCC warnings

--- src/CellularAutomataSolver.java
+++ src/CellularAutomataSolver.java
@@ -164,4 +164,4 @@ public class CellularAutomataSolver {
             }catch(Exception e){}
             Point[] result = new Point[pos.size()];
             int v =0;
-          
+            while(!pos.isEmpty(
