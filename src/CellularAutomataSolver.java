349a03a65d53a811cf32c6e0acbf4731c2ef2c07
libcolor <97296222+libcolor@users.noreply.github.com>
2023-03-19 17:49:52 +0000

    Add package files

--- src/CellularAutomataSolver.java
+++ src/CellularAutomataSolver.java
@@ -225,4 +225,4 @@ public class CellularAutomataSolver {
         for(int i =0;i<mat.length;i++){
             for(int j = 0;j< mat[i].length;j++){
                 if(mat[i][j].getBackground()==Color.GRAY){
-                   
+                    maze.repaintSin
